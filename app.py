cd321066c4d91156004601a1a5f0746319aec936
Federico Arduini <federicoarduini40@gmail.com>
2024-05-16 16:00:03 +0200

    flask test

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+@app.route('/sium')
+def sium():
+    return render_template('sium.html')
 
 
 if __name__ == '__main__':
